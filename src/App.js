7fd5375fd04eec5b22c630d525bfb1fa82fc30b8
Arobl034 <anthonyrobles@Anthonys-iMac.attlocal.net>
2020-05-25 22:17:43 -0400

    pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page3 from './pages/Page3'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+
+class SampleClassComponent extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      name: "Keyla, Anthony, Omar, Jonathan"
+    }
+  }
+changeName=()=> {
+  this.setState({
+    name: "Who-Help"
+  })
+}
+
+
+  render() {
+  return <div>You are not alone {this.props.value} live chats avialable;
+  <div>Members: {this.state.name}</div>
+  <button onClick={this.changeName}>Team Name</button>
+  </div>
+  }
+}
+
+function Page1 (props) {
+  return <div>Page 1</div>
+}
+function Page2 (props) {
+  return <div>Page 2</div>
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+    
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          You Are Not Alone
-        </p>
-        <p>
-          Help Around The Corner
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Team: Self-Help
+          Team: Who-Help
         </a>
+        <Router>
+          <Switch>
+        <Route path="/page1" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        <Route path="/page3" component={Page3} />
+        <Route component={SampleClassComponent} />
+        </Switch>
+
+        </Router>
       </header>
     </div>
   );
